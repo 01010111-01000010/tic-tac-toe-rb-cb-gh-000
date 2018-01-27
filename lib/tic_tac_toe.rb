85c9fb83937c231771ff1a2145b17828818a04d8
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-27 04:59:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,8 +49,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn = turn_count(board)
-  if turn % 2 == 0
+  turns = turn_count(board)
+  if turns % 2 == 0
     return "X"
   else
     return "O"
